2585fa0ce024924dd2cdec927bd835ebd106067d
Velyn-N <nils01.gaming@gmail.com>
2022-04-05 19:40:13 +0200

    Added Quasar Project

---
+++ ausbildungsnachweise-quasar/README.md
@@ -0,0 +1,24 @@
+# Ausbildungsnachweise (ausbildungsnachweise)
+
+Tool zum Führen, Verwalten und Exportieren von Ausbildungsnachweisen der IHK.
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
---
+++ ausbildungsnachweise-quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Ausbildungsnachweise
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="bg-dark">
+      <router-view />
+    </q-page-container>
+    <q-footer class="row q-pt-sm q-pl-md q-pr-md q-pb-sm">
+      <div class="col text-left">
+        <a class="text-grey-4" href="https://github.com/Velyn-N/ausbildungsnachweise">Ausbildungsnachweise</a>
+        by
+        <a class="text-grey-4" href="https://github.com/Velyn-N">Velyn-N</a>
+      </div>
+      <div class="col text-right">
+        Right
+      </div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+</script>
